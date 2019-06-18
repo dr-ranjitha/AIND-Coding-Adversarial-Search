d6287b46993b504b0e48cec8427be5c760a4d036
dr-ranjitha <51212846+dr-ranjitha@users.noreply.github.com>
2019-06-18 13:39:48 -0600

    minimax_helpers.py uploaded

---
+++ minimax_helpers.py
@@ -0,0 +1,20 @@
+def min_value(gameState):
+    """ Return the game state utility if the game is over,
+    otherwise return the minimum value over all legal successors
+    
+    # HINT: Assume that the utility is ALWAYS calculated for
+            player 1, NOT for the "active" player
+    """
+    # TODO: finish this function!
+    pass
+
+
+def max_value(gameState):
+    """ Return the game state utility if the game is over,
+    otherwise return the maximum value over all legal successors
+    
+    # HINT: Assume that the utility is ALWAYS calculated for
+            player 1, NOT for the "active" player
+    """
+    # TODO: finish this function!
+    pass
